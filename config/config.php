87735a2a7a43c73a2336babf5db99a91420143cc
szoGGun <przrut4@wp.pl>
2021-07-29 22:07:22 +0200

    Deleted one dump and added little comment to config file

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return [
     'db' => [
+        // It's just jawsDB for herokuapp. Nothing confidential :D.
         'host' => 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306',
         'database' => 'q333xdd1huxfl0hq',
         'user' => 'y3p4bsywft2j9q45',
