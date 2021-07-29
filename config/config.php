70b1f36845c5096abb85602dc5a19954d873f893
szoGGun <57073415+szoGGun@users.noreply.github.com>
2021-07-29 21:56:46 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 return [
     'db' => [
-        'host' => 'xxx',
-        'database' => 'xxx',
-        'user' => 'xxx',
-        'password' => 'xxx'
+        'host' => 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306',
+        'database' => 'q333xdd1huxfl0hq',
+        'user' => 'y3p4bsywft2j9q45',
+        'password' => 'ujum4j4b30dg6hcr'
     ]
 ];
